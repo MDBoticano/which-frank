352bcb6bfdc990089b5e5f41bb2433acb0c69504
MDBoticano <MDBoticano@users.noreply.github.com>
2019-10-21 16:14:26 -0700

    Fixed loopin when going home. Cleanup for release 1.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 
 /* data imported, since no backend */
 import FrankLyrics from './data/FrankLyrics';
@@ -6,8 +7,8 @@ import FrankLyrics from './data/FrankLyrics';
 
 /* Helper functions */
 // import { getUniqueValues, shuffleIndices } from './utilities/helperFuncs';
-import { 
-  getUniqueValues, shuffleIndices, reorderArray, sumReducer, 
+import {
+  getUniqueValues, shuffleIndices, reorderArray, sumReducer,
 } from './utilities/helperFuncs';
 
 /* ------------------------------- Components ------------------------------- */
@@ -21,52 +22,39 @@ const GAME = 'game';
 const SCORE = 'score';
 
 // Loading
-const Loading = () => {
-  return (
-    <div className="Loading">
-      ...loading
-    </div>
-  );
-};
+const Loading = () => (
+  <div className="Loading">
+    ...loading
+  </div>
+);
 
-// Home
-const Home = (props) => {
-  return (
-    <div className="Home">
-      Home
-      <button type="button" onClick={() => props.setPage(GAME)}>
-        Let's Play!
-      </button>
-    </div>
-  );
+/* ---------- Page: Home ---------- */
+const Home = (props) => (
+  <div className="Home">
+    <p>Home</p>
+    <button type="button" onClick={() => props.setPage(GAME)}>
+      {'Let\'s Play!'}
+    </button>
+  </div>
+);
+
+Home.propTypes = {
+  setPage: PropTypes.func.isRequired,
 };
+/* -------- End Page: Home -------- */
 
 /* ---------- Page: Game ---------- */
 const Game = (props) => {
   /* ----- State: Game ----- */
   const [activeLyric, setActiveLyric] = useState({});
   const [numGuesses, setNumGuesses] = useState(props.score.length);
-  const [scorePrompt, setScorePrompt] = useState(false);
-
+  const [showScorePrompt, setShowScorePrompt] = useState(false);
   /* --- End State: Game --- */
 
   /* ----- Helpers: Game ----- */
   const addScore = (value) => {
     props.setScore([...props.score, value]);
   };
-
-  const showScorePrompt = () => {
-    setScorePrompt(true);
-  }
-
-  const makeGuessButtons = (artistsList) => {
-    const artistsButtons = artistsList.map((artist) => (
-      <button type="submit" key={artist} onClick={() => checkGuess(artist)}>
-        {artist}
-      </button>
-    ));
-    return artistsButtons;
-  }
 
   const checkGuess = (guess) => {
     if (guess === activeLyric.artistName) {
@@ -76,36 +64,50 @@ const Game = (props) => {
     }
     const nextIndex = numGuesses + 1;
     if (nextIndex === props.allLyrics.length) {
-      showScorePrompt();
+      setShowScorePrompt(true);
     } else {
       setNumGuesses(nextIndex);
       setActiveLyric(props.allLyrics[nextIndex]);
     }
-  }
+  };
+
+  const makeGuessButtons = (artistsList) => {
+    const artistsButtons = artistsList.map((artist) => (
+      <button type="submit" key={artist} onClick={() => checkGuess(artist)}>
+        {artist}
+      </button>
+    ));
+    return artistsButtons;
+  };
+
+  const viewScorePage = () => {
+    props.setPage(SCORE);
+    setShowScorePrompt(false);
+  };
   /* --- End Helpers: Game --- */
 
   /* ----- Hooks: Game ----- */
   useEffect(() => {
     if (props.allLyrics.length > 0) {
       setActiveLyric(props.allLyrics[0]);
-    } 
+    }
   }, [props.allLyrics]);
   /* --- End Hooks: Game --- */
 
-  if (scorePrompt) {
+  if (showScorePrompt) {
     return (
       <div className="Game">
-        All Done!
-        <button type="button" onClick={() => props.setPage(SCORE)}>
+        <p>All Done!</p>
+        <button type="button" onClick={() => viewScorePage()}>
           View Score
         </button>
       </div>
-    )
+    );
   }
 
   return (
     <div className="Game">
-      Game
+      <p>Game</p>
       <div className="ActiveLyric">
         Lyric:
         {activeLyric.songLyric}
@@ -114,25 +116,38 @@ const Game = (props) => {
     </div>
   );
 };
+
+Game.propTypes = {
+  score: PropTypes.arrayOf(PropTypes.number).isRequired,
+  setScore: PropTypes.func.isRequired,
+  setPage: PropTypes.func.isRequired,
+  allLyrics: PropTypes.arrayOf(PropTypes.shape({
+    songLyric: PropTypes.string.isRequired,
+    songName: PropTypes.string.isRequired,
+    artistName: PropTypes.string.isRequired,
+    albumName: PropTypes.string.isRequired,
+    albumReleaseDate: PropTypes.string.isRequired,
+  })).isRequired,
+  allArtists: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
 /* -------- End Page: Game -------- */
 
 /* ---------- Page: Score ---------- */
 const Score = (props) => {
-/* ----- Helpers: Score ----- */
-const reduceScore = (scoreArray) => {
-   /* Note: import sumReducer in component */
-  const numCorrect = sumReducer(scoreArray);
-  const maxCorrect = scoreArray.length;
-  return `You score ${numCorrect} out of ${maxCorrect}`;
-};
+  /* ----- Helpers: Score ----- */
+  const reduceScore = (scoreArray) => {
+    /* Note: import sumReducer in component */
+    const numCorrect = sumReducer(scoreArray);
+    const maxCorrect = scoreArray.length;
+    return `You score ${numCorrect} out of ${maxCorrect}`;
+  };
 
-const resetGame = () => {
-  props.setPage(GAME);
-  props.setScore([]);
-  props.reshuffleLyrics();
-}
-
-/* --- End Helpers: Score --- */
+  const resetGameAndView = (page) => {
+    props.setPage(page);
+    props.setScore([]);
+    props.reshuffleLyrics();
+  };
+  /* --- End Helpers: Score --- */
 
   return (
     <div className="Score">
@@ -140,18 +155,24 @@ const resetGame = () => {
         Score:
         {reduceScore(props.score)}
       </p>
-      <button type="button" onClick={() => props.setPage(HOME)}>
+      <button type="button" onClick={() => resetGameAndView(HOME)}>
         Home
       </button>
-      <button type="button" onClick={() => resetGame()}>
+      <button type="button" onClick={() => resetGameAndView(GAME)}>
         Play Again
       </button>
     </div>
   );
 };
+
+Score.propTypes = {
+  score: PropTypes.arrayOf(PropTypes.number).isRequired,
+  setPage: PropTypes.func.isRequired,
+  setScore: PropTypes.func.isRequired,
+  reshuffleLyrics: PropTypes.func.isRequired,
+};
 /* -------- End Page: Score -------- */
 /* ------------------ End Pages ------------------ */
-
 /* ----------------------------- End Components ----------------------------- */
 
 const App = () => {
@@ -165,10 +186,9 @@ const App = () => {
   /* helpers */
   const reshuffleLyrics = () => {
     const shuffledLyricsOrder = shuffleIndices(allLyrics.length);
-    // console.log('new order:', shuffledLyricsOrder);
     const newShuffledLyrics = reorderArray(FrankLyrics, shuffledLyricsOrder);
     setShuffledLyrics(newShuffledLyrics);
-  }
+  };
 
   /* load lyrics from 'backend' */
   useEffect(() => {
@@ -180,12 +200,11 @@ const App = () => {
 
       // shuffle and set the retrieved lyrics
       const shuffledLyricsOrder = shuffleIndices(lyricsLength);
-      // console.log('first order:', shuffledLyricsOrder);
-      const newShuffledLyrics = reorderArray(FrankLyrics, shuffledLyricsOrder);      
+      const newShuffledLyrics = reorderArray(FrankLyrics, shuffledLyricsOrder);
       setShuffledLyrics(newShuffledLyrics);
 
       // get list of all artists
-      const uniqueArtists = getUniqueValues(FrankLyrics, "artistName");
+      const uniqueArtists = getUniqueValues(FrankLyrics, 'artistName');
       setAllArtists(uniqueArtists);
     }
   }, []);
@@ -193,14 +212,8 @@ const App = () => {
 
   return (
     <div className="App">
-      {/* shouldn't use this Nav once routing is set up */}
-      {/* <div className="Nav">
-        <button type="button" onClick={() => setPage(HOME)}>Home</button>
-        <button type="button" onClick={() => setPage(GAME)}>Play</button>
-        <button type="button" onClick={() => setPage(SCORE)}>Score</button>
-      </div> */}
       {(() => {
-        switch(page) {
+        switch (page) {
           case LOADING:
             return (
               <Loading
@@ -233,7 +246,7 @@ const App = () => {
                 reshuffleLyrics={reshuffleLyrics}
               />
             );
-          default: 
+          default:
             return null;
         }
       })()}
