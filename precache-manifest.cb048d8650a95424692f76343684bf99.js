3fb8a859366d65e22472ea1e42f3fd0b9fe1dfa0
mdboticano <mdboticano@gmail.com>
2019-10-22 10:37:35 -0700

    Updates

---
+++ precache-manifest.cb048d8650a95424692f76343684bf99.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e00f7486f6122ee30e7ae7eef5841dc5",
+    "url": "/which-frank/index.html"
+  },
+  {
+    "revision": "f121fdbebcf18431f207",
+    "url": "/which-frank/static/js/2.44221d9b.chunk.js"
+  },
+  {
+    "revision": "998f7886518a8df3ade8",
+    "url": "/which-frank/static/js/main.8842788d.chunk.js"
+  },
+  {
+    "revision": "92de527246133f4b6b35",
+    "url": "/which-frank/static/js/runtime-main.41db6163.js"
+  }
+]);
