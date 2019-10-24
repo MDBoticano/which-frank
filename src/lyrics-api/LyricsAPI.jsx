bb13cfe6aa74d7c0bba4d1a6424dd6b24af3ed84
MDBoticano <MDBoticano@users.noreply.github.com>
2019-10-24 15:48:40 -0700

    Can now retrieve top tracks

--- src/lyrics-api/LyricsAPI.jsx
+++ src/lyrics-api/LyricsAPI.jsx
@@ -1,50 +1,30 @@
 import React, { useState, useEffect } from 'react';
 import { 
   getArtistList,
+  getTopTracks,
 } from './lyrics-services';
 
 const API_KEY = process.env.REACT_APP_MUSIXMATCH_API_KEY;
 // console.log(process.env.NODE_ENV);
 // console.log(API_KEY);
 
-
-
 const LyricsAPI = (props) => {
   const [artistsData, setArtistsData] = useState([]);
-  // const [artistSongs, setArtistSongs] = useState([]);
+  const [artistSongs, setArtistSongs] = useState([]);
   const [loading ,setLoading] = useState(true);
 
   useEffect(() => {
-    const fetchArtists = async () => {
+    const fetchArtists = () => {
       getArtistList('frank ocean', 1, setArtistsData, API_KEY);
     }
 
-    // const fetchTopSongs = async (artists, numSongs) => {
-    //   const artist = artists[0].artist.artist_name
-
-
-    //   /* extract artist names from list */
-    //   const QUERY = `track.search?q_artist=${artist}&page_size=${numSongs}&s_track_rating=desc`;
-    //   const API_KEY_PARAM = `&apikey=${API_KEY}`;
-
-    //   /* get one artist's songs */ 
-
-    //   /* turn function into a loop */
-    
-    //   const requestURL = `${CORS_PROXY}${API}${QUERY}${API_KEY_PARAM}`;
-
-    //   const result = await axios(requestURL);
-
-    //   setArtistSongs(result);
-    // }
+    const fetchTopTracks = () => {
+      getTopTracks('frank ocean', 5, setArtistSongs, API_KEY);
+    }
 
     if (window.confirm('Query?')) {
       fetchArtists();
-      // fetchTopSongs([{
-      //   artist: {
-      //     artist_name: "Frank Ocean",
-      //   }
-      // }], 5);
+      fetchTopTracks();
     }
 
     setLoading(false);
@@ -58,12 +38,18 @@ const LyricsAPI = (props) => {
     <div className="LyricsAPI">
       <p>Lyrics. Yeah!</p>
       <p>Query Result:</p>
-      {/* {displayArtists(artistsData)} */}
       {displayLoading(loading)}
       <ul>
         {artistsData.map(artist => (
-          <li key={artist.artistId}>
-            {artist.artistName} -- {artist.artistId}
+          <li key={artist.artist_id}>
+            {artist.artist_name} -- {artist.artist_id}
+          </li>
+        ))}
+      </ul>
+      <ul>
+        {artistSongs.map(song => (
+          <li key={song.track_id}>
+            {song.track_name} -- {song.album_name}
           </li>
         ))}
       </ul>
--- src/lyrics-api/lyrics-services.js
+++ src/lyrics-api/lyrics-services.js
@@ -6,25 +6,23 @@ const CORS_PROXY = "https://cors-anywhere.herokuapp.com/";
 const API = "https://api.musixmatch.com/ws/1.1/";
 
 /**
- * @param {name} string - name of artists to look for
- * @param {setter} func - state modifier function to call
+ * @param {artist} string - name of artists to look for
  * @param {numResults} number - number of artists to retreive
- * @return [ { artistNmame: string, artistId: number }, ... ]
+ * @param {setter} func - state modifier function to call
+ * @param {api_key} string - Musixmatch API key
  */
-export const getArtistList = async (name, numResults, setter, api_key) => {
-  const QUERY = `artist.search?q_artist=${name}&page_size=${numResults}`;
+export const getArtistList = async (artist, numResults, setter, api_key) => {
+  const QUERY = `artist.search?q_artist=${artist}&page_size=${numResults}`;
   const API_KEY = `&apikey=${api_key}`;
-
+  
   const requestURL = `${CORS_PROXY}${API}${QUERY}${API_KEY}`;
-
   const result = await axios(requestURL);
   
   const artistList = result.data.message.body.artist_list;
-  // console.log(artistList);
   const simpleArtistList = artistList.map(artist => (
     { 
-      artistName: artist.artist.artist_name,
-      artistId: artist.artist.artist_id,
+      artist_name: artist.artist.artist_name,
+      artist_id: artist.artist.artist_id,
     }
   ))
   console.log(simpleArtistList);
@@ -32,5 +30,30 @@ export const getArtistList = async (name, numResults, setter, api_key) => {
 }
 
 /**
- * 
+ * @param {artist} string - name of artists to look for
+ * @param {numResults} number - number of tracks to retreive
+ * @param {setter} func - state modifier function to call
+ * @param {api_key} string - Musixmatch API key
  */
+export const getTopTracks = async (artist, numResults, setter, api_key) => {
+  const QUERY = `track.search?q_artist=${artist}`;
+  const MODIFIERS = `&page_size=${numResults}&s_track_rating=desc`;
+  const API_KEY = `&apikey=${api_key}`;
+
+  const requestURL = `${CORS_PROXY}${API}${QUERY}${MODIFIERS}${API_KEY}`;
+  const result = await axios(requestURL);
+  console.log(result);
+
+  const trackList = result.data.message.body.track_list;
+  const simplerTrackList = trackList.map(track => (
+    {
+      track_name: track.track.track_name,
+      track_id: track.track.track_id,
+      album_name: track.track.album_name,
+      album_id: track.track.album_id,
+    }
+  ))
+
+  console.log(simplerTrackList);
+  setter(simplerTrackList);
+}
