563f61cfb8945dd8da3442bf32af1ea819890075
mdboticano <mdboticano@gmail.com>
2019-11-06 15:40:42 -0800

    Updates

---
+++ precache-manifest.f387fb896db9a368ada86d1d99275fd6.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02ed4da82915fee1c55376996743adda",
+    "url": "/which-frank/index.html"
+  },
+  {
+    "revision": "12f990b8b8e49122e5ab",
+    "url": "/which-frank/static/js/2.3d479ca1.chunk.js"
+  },
+  {
+    "revision": "3caf34c1bd5d56f2fae1",
+    "url": "/which-frank/static/js/main.bf531a45.chunk.js"
+  },
+  {
+    "revision": "92de527246133f4b6b35",
+    "url": "/which-frank/static/js/runtime-main.41db6163.js"
+  }
+]);
