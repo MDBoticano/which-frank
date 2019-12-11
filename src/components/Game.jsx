0241175db88f7b92a0c0f91f3a45c42d4796eb96
Marlo Boticano <m.boticano@gmail.com>
2019-12-10 16:46:10 -0800

    Feature: Core guessing functionality implemented
    - Queried lyrics are displayed one by one
    - Buttons are made from possible artists
    - Once all lyrics have been seen, prompt score page

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -4,33 +4,72 @@ import { Link } from 'react-router-dom';
 /* context */
 import DataContext from '../DataContext';
 
+/* utilities */
+import { getUniqueValues } from '../utilities/helperFuncs';
+
 const Game = () => {
   const dataContext = useContext(DataContext);
-  const SCORE = dataContext.score;
+  const snippets = dataContext.snippets;
+  const numSnippets = snippets.length;
+  /* Instead of using context artist values, use artist names frm snippets
+   * This is because the query may not match the spelling used in the API
+   */ 
+  const artists = getUniqueValues(snippets, 'artist_name');
 
-  const [gameScore, setGameScore] = useState(SCORE);
+  const [gameScore, setGameScore] = useState(0);
+  const [snipIndex, setSnipIndex] = useState(0);
+
+  const submitGuess = (guess) => {
+    const currentArtist = snippets[snipIndex].artist_name;
+    if (guess === currentArtist) {
+      setGameScore(gameScore + 1);
+    }
+    setSnipIndex(snipIndex + 1);
+  }
+
+  const makeGuessButtons = (artists) => {
+    const buttons = artists.map((artist) => {
+      return (
+        <button key={artist} onClick={() => submitGuess(artist)}>
+          {artist}
+        </button>
+      );
+    });
+    return buttons;
+  }
+
+
+  const displaySnippet = (snip) => {
+    return (
+      <div className="snippet">
+        <p>{snip.snippet}</p>
+        {makeGuessButtons(artists)}
+      </div>
+    );
+  }
+
+  const displayScorePrompt = () => {
+    return (
+      <div className="score-prompt">
+        <p>That's all of them!</p>
+        <Link to="/score">
+        <button onClick={() => dataContext.setScore(gameScore)}>
+          View Score
+        </button>
+      </Link>
+      </div>
+    );
+  }
 
   return (
     <div className="game">
       {console.log('<Game/> is rendered')}
 
-      GAME
+      <h1>GAME</h1>
+      <h4>score: {gameScore}</h4>
 
-      score: {gameScore}
-
-      <button onClick={() => {setGameScore(gameScore + 1)}}>
-        Correct Answer
-      </button>
-
-      <button>
-        Wrong Answer
-      </button>
-
-      <Link to="/score">
-        <button onClick={() => dataContext.setScore(gameScore)}>
-          View Score
-        </button>
-      </Link>
+      {snipIndex < numSnippets && displaySnippet(snippets[snipIndex])}
+      {snipIndex >= numSnippets && displayScorePrompt()}
     </div>
   );
 }
